8702d7cb5081ade578b81698010972ac535a0eca
Rucveda <sean2371@naver.com>
2026-05-17 21:52:36 +0900

    error patch

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,6 @@ import { defineConfig } from "@prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env["DIRECT_URL"], // 💡 패치: 터미널 명령어(db push)는 5432 포트를 사용하도록 변경
+    url: process.env["DIRECT_URL"] || process.env["DATABASE_URL"], // Render는 보통 DATABASE_URL만 제공합니다.
   },
 });
